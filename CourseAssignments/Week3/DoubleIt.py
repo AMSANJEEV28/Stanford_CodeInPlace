3efc4c9e431078235882a5b92b2b2147058ee47d
A M SANJEEV <amsanjeev28@gmail.com>
2023-05-14 04:05:27 +0530

    Week 3

---
+++ CourseAssignments/Week3/DoubleIt.py
@@ -0,0 +1,11 @@
+def main():
+    """
+    You should write your code here. 
+    """
+    curr_value = int(input("Enter a number: "))
+    while curr_value<100:
+        curr_value=curr_value*2
+        print(curr_value)
+
+if __name__ == '__main__':
+    main()
