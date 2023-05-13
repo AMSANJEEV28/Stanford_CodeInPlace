910176fa12a84f404e2e0fed792a5f7c78038182
A M SANJEEV <amsanjeev28@gmail.com>
2023-05-14 03:34:45 +0530

    Week 3

---
+++ CourseAssignments/Week3/JokeBot.py
@@ -0,0 +1,18 @@
+PROMPT = "What do you want? "
+JOKE = "Here is a joke for you! Karel is heading out to the grocery store. A programmer tells her: get a liter of milk, and if they have eggs, get 12. Karel returns with 13 liters of milk. The programmer asks why and Karel replies: 'because they had eggs'"
+SORRY = "Sorry I only tell jokes"
+
+def main():
+    user_input = input(PROMPT)
+    # your code here.
+    if user_input=="joke":
+        print(JOKE)
+    elif user_input=="JOKE":
+        print(JOKE)
+    elif user_input=="Joke":
+        print(JOKE)
+    else:
+        print(SORRY)
+
+if __name__ == "__main__":
+    main()
