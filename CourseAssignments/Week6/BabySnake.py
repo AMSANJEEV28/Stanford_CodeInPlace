021d8f8684832c89269926a93d22a7b755da3b35
A M SANJEEV <amsanjeev28@gmail.com>
2023-05-30 09:42:11 +0530

    Milestone 1

---
+++ CourseAssignments/Week6/BabySnake.py
@@ -0,0 +1,30 @@
+from graphics import Canvas
+import time
+import random
+    
+CANVAS_WIDTH = 400
+CANVAS_HEIGHT = 400
+SIZE = 20
+
+# if you make this larger, the game will go slower
+DELAY = 0.1 
+
+def main():
+    canvas = Canvas(CANVAS_WIDTH, CANVAS_HEIGHT)
+    
+    # TODO: your code here
+    
+    x = random.randint(0, CANVAS_HEIGHT-SIZE)
+    y = random.randint(0, CANVAS_WIDTH-SIZE)    
+    start_x=0
+    start_y=0
+    
+    player=canvas.create_rectangle(start_x, start_y, start_x+SIZE,start_y+SIZE,"blue")
+    snake=canvas.create_rectangle(x, y, x + SIZE, y + SIZE,"red")    
+        
+        
+    
+        
+
+if __name__ == '__main__':
+    main()
