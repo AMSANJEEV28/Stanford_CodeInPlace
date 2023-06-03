07c8157cdf6ebf5a6bde6d583b0eb09d6bb348ce
A M SANJEEV <amsanjeev28@gmail.com>
2023-06-03 18:40:07 +0530

    Section Week 6

---
+++ SectionAssignments/Week6/HeadsUp.py
@@ -0,0 +1,32 @@
+import random
+
+# Name of the file to read in!
+FILE_NAME = 'cswords.txt'
+
+def get_words_from_file():
+    """
+    This function has been implemented for you. It opens a file, 
+    and stores all of the lines into a list of strings. 
+    It returns a list of all lines in the file. 
+    """
+    f = open(FILE_NAME)
+    lines = []
+    for line in f:
+        # removes whitespace characters (\n) from the start and end of the line
+        line = line.strip() 
+        # if the line was only whitespace characters, skip it 
+        if line != "":
+            lines.append(line)
+    return lines
+
+def play_game(words):
+    while True:
+        random_word = random.choice(words)
+        input(random_word)
+
+def main():
+    words = get_words_from_file()
+    play_game(words)
+
+if __name__ == '__main__':
+    main()
